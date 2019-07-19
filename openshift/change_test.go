e61d5b9b13e5ab2eaea3ea17e58fe3d163a84855
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2019-07-19 14:33:09 +0200

    Fix incorrect formatting

--- openshift/change_test.go
+++ openshift/change_test.go
@@ -260,7 +260,7 @@ func TestDiff(t *testing.T) {
 		actualPatches := change.Patches
 		if !reflect.DeepEqual(actualPatches, tt.expectedPatches) {
 			t.Errorf(
-				"Diff()\n===== expected =====\n%s\n===== actual =====\n%s",
+				"Diff()\n===== expected =====\n%v\n===== actual =====\n%v",
 				tt.expectedPatches,
 				actualPatches,
 			)
