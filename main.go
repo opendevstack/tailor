2525bb3847926d28a54396ca4279cdb41048c65b
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2019-07-22 17:05:55 +0200

    Set master version to 0.9.5+master

--- main.go
+++ main.go
@@ -211,7 +211,7 @@ func main() {
 	command := kingpin.MustParse(app.Parse(os.Args[1:]))
 
 	if command == versionCommand.FullCommand() {
-		fmt.Println("0.9.5")
+		fmt.Println("0.9.5+master")
 		return
 	}
 
