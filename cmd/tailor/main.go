1a4276b4b1db7be5f611067b95a7d7a5bf5ad8eb
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2019-09-30 10:29:04 +0200

    Set master version to 0.10.0+master

--- cmd/tailor/main.go
+++ cmd/tailor/main.go
@@ -237,7 +237,7 @@ func main() {
 	command := kingpin.MustParse(app.Parse(os.Args[1:]))
 
 	if command == versionCommand.FullCommand() {
-		fmt.Println("0.10.0")
+		fmt.Println("0.10.0+master")
 		return
 	}
 
