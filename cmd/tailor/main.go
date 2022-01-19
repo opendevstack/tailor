255a7959981e896e3835c69782cec45881183e4c
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2022-01-19 17:14:58 +0100

    Bump version to 1.3.4

--- cmd/tailor/main.go
+++ cmd/tailor/main.go
@@ -253,7 +253,7 @@ func main() {
 	command := kingpin.MustParse(app.Parse(os.Args[1:]))
 
 	if command == versionCommand.FullCommand() {
-		fmt.Println("1.3.3+master")
+		fmt.Println("1.3.4")
 		return
 	}
 
